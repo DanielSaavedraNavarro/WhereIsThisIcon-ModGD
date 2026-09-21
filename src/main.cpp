154a209d51905283e99129f2f71c38b0385014e3
DanielSaavedraNavarro <danielsn.contacto@gmail.com>
2026-09-21 16:32:24 -0300

    Actualizar main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,107 +3,38 @@
 
 using namespace geode::prelude;
 
-class WhereIsThisIconLayer : public FLAlertLayer {
-protected:
-    bool init() {
-        if (!FLAlertLayer::init(
-            nullptr,
-            "WHERE IS THIS ICON?",
-            "Find this icon somewhere in Geometry Dash!",
-            "OK",
-            nullptr,
-            320.f,
-            false,
-            200.f,
-            1.f
-        )) {
-            return false;
-        }
-
-        return true;
-    }
-
-public:
-    static WhereIsThisIconLayer* create() {
-        auto ret = new WhereIsThisIconLayer();
-
-        if (ret && ret->init()) {
-            ret->autorelease();
-            return ret;
-        }
-
-        CC_SAFE_DELETE(ret);
-        return nullptr;
-    }
-};
-
-
-class $modify(MenuLayer) {
+class $modify(WhereIsThisIconMenuLayer, MenuLayer) {
     bool init() {
         if (!MenuLayer::init())
             return false;
 
-        // Obtener el menú donde están los botones inferiores
         auto menu = this->getChildByID("bottom-menu");
 
-        if (!menu) {
-            log::error("WHERE IS THIS ICON?: bottom-menu not found!");
-            return true;
+        if (menu) {
+            auto sprite = CCSprite::createWithSpriteFrameName("GJ_infoIcon_001.png");
+
+            if (sprite) {
+                auto button = CCMenuItemSpriteExtra::create(
+                    sprite,
+                    this,
+                    menu_selector(WhereIsThisIconMenuLayer::onWhereIsThisIcon)
+                );
+
+                button->setID("where-is-this-icon-button"_spr);
+
+                menu->addChild(button);
+                menu->updateLayout();
+            }
         }
-
-        // Imagen normal
-        auto normalSprite = CCSprite::create("iconMenu.png"_spr);
-
-        if (!normalSprite) {
-            log::error("WHERE IS THIS ICON?: iconMenu.png could not be loaded!");
-            return true;
-        }
-
-        // Imagen al hacer hover/click
-        auto selectedSprite = CCSprite::create("iconMenu.png"_spr);
-
-        if (!selectedSprite) {
-            selectedSprite = normalSprite;
-        }
-
-        // Un pequeño efecto de hover
-        selectedSprite->setScale(1.10f);
-
-        auto button = CCMenuItemSpriteExtra::create(
-            normalSprite,
-            selectedSprite,
-            this,
-            menu_selector(MenuLayer::onWhereIsThisIcon)
-        );
-
-        if (!button) {
-            log::error("WHERE IS THIS ICON?: failed to create button!");
-            return true;
-        }
-
-        button->setID("where-is-this-icon-button"_spr);
-
-        // Añadirlo junto a los otros botones
-        menu->addChild(button);
-
-        // Dejar que el layout del menú lo acomode
-        menu->updateLayout();
-
-        log::info("WHERE IS THIS ICON?: button added!");
 
         return true;
     }
 
     void onWhereIsThisIcon(CCObject*) {
-        auto layer = WhereIsThisIconLayer::create();
-
-        if (layer) {
-            layer->show();
-        }
+        FLAlertLayer::create(
+            "Where Is This Icon?",
+            "¡El botón funciona! :D",
+            "OK"
+        )->show();
     }
 };
-
-
-$on_mod(Loaded) {
-    log::info("WHERE IS THIS ICON? loaded!");
-}
