e3cb74dcf5c7e60846f4d61aa5e79e14f4059f04
DanielSaavedraNavarro <danielsn.contacto@gmail.com>
2026-09-21 16:14:58 -0300

    Actualizar main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Geode/Geode.hpp>
+#include <Geode/modify/MenuLayer.hpp>
 
 using namespace geode::prelude;
 
@@ -36,12 +37,73 @@ public:
     }
 };
 
+
+class $modify(MenuLayer) {
+    bool init() {
+        if (!MenuLayer::init())
+            return false;
+
+        // Obtener el menú donde están los botones inferiores
+        auto menu = this->getChildByID("bottom-menu");
+
+        if (!menu) {
+            log::error("WHERE IS THIS ICON?: bottom-menu not found!");
+            return true;
+        }
+
+        // Imagen normal
+        auto normalSprite = CCSprite::create("iconMenu.png"_spr);
+
+        if (!normalSprite) {
+            log::error("WHERE IS THIS ICON?: iconMenu.png could not be loaded!");
+            return true;
+        }
+
+        // Imagen al hacer hover/click
+        auto selectedSprite = CCSprite::create("iconMenu.png"_spr);
+
+        if (!selectedSprite) {
+            selectedSprite = normalSprite;
+        }
+
+        // Un pequeño efecto de hover
+        selectedSprite->setScale(1.10f);
+
+        auto button = CCMenuItemSpriteExtra::create(
+            normalSprite,
+            selectedSprite,
+            this,
+            menu_selector(MenuLayer::onWhereIsThisIcon)
+        );
+
+        if (!button) {
+            log::error("WHERE IS THIS ICON?: failed to create button!");
+            return true;
+        }
+
+        button->setID("where-is-this-icon-button"_spr);
+
+        // Añadirlo junto a los otros botones
+        menu->addChild(button);
+
+        // Dejar que el layout del menú lo acomode
+        menu->updateLayout();
+
+        log::info("WHERE IS THIS ICON?: button added!");
+
+        return true;
+    }
+
+    void onWhereIsThisIcon(CCObject*) {
+        auto layer = WhereIsThisIconLayer::create();
+
+        if (layer) {
+            layer->show();
+        }
+    }
+};
+
+
 $on_mod(Loaded) {
     log::info("WHERE IS THIS ICON? loaded!");
-
-    auto layer = WhereIsThisIconLayer::create();
-
-    if (layer) {
-        layer->show();
-    }
 }
